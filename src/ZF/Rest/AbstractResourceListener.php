a6c9f2a010e0e8ff1a6adeece61091e0a3107564
Matthew Weier O'Phinney <matthew@zend.com>
2013-07-29 10:45:24 -0500

    Implemented default listener
    - Created a default listener. It acts like the Resource object, but
      listens to the Resource events. A special "dispatch" method composes
      the incoming event, marshals arguments, and executes the appropriate
      method.

---
+++ src/ZF/Rest/AbstractResourceListener.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-2-Clause BSD-2-Clause
+ */
+
+namespace ZF\Rest;
+
+use Zend\EventManager\AbstractListenerAggregate;
+use Zend\EventManager\EventManagerInterface;
+use ZF\ApiProblem\ApiProblem;
+
+abstract class AbstractResourceListener extends AbstractListenerAggregate
+{
+    /**
+     * @var ResourceEvent
+     */
+    protected $event;
+
+    /**
+     * Retrieve the current resource event, if any
+     * 
+     * @return ResourceEvent
+     */
+    public function getEvent()
+    {
+        return $this->event;
+    }
+
+    /**
+     * Attach listeners for all Resource events
+     * 
+     * @param  EventManagerInterface $events 
+     */
+    public function attach(EventManagerInterface $events)
+    {
+        $events->attach('create',      array($this, 'dispatch'));
+        $events->attach('delete',      array($this, 'dispatch'));
+        $events->attach('deleteList',  array($this, 'dispatch'));
+        $events->attach('fetch',       array($this, 'dispatch'));
+        $events->attach('fetchAll',    array($this, 'dispatch'));
+        $events->attach('patch',       array($this, 'dispatch'));
+        $events->attach('replaceList', array($this, 'dispatch'));
+        $events->attach('update',      array($this, 'dispatch'));
+    }
+
+    /**
+     * Dispatch an incoming event to the appropriate method
+     *
+     * Marshals arguments from the event parameters.
+     * 
+     * @param  ResourceEvent $event 
+     * @return mixed
+     */
+    public function dispatch(ResourceEvent $event)
+    {
+        $this->event = $event;
+        switch ($event->getName()) {
+            case 'create':
+                $data = $event->getParam('data', array());
+                return $this->create($data);
+            case 'delete':
+                $id   = $event->getParam('id', null);
+                return $this->delete($id);
+            case 'deleteList':
+                $data = $event->getParam('data', array());
+                return $this->deleteList($data);
+            case 'fetch':
+                $id   = $event->getParam('id', null);
+                return $this->fetch($id);
+            case 'fetchAll':
+                return $this->fetchAll($event->getParams());
+            case 'patch':
+                $id   = $event->getParam('id', null);
+                $data = $event->getParam('data', array());
+                return $this->patch($id, $data);
+            case 'replaceList':
+                $data = $event->getParam('data', array());
+                return $this->replaceList($data);
+            case 'update':
+                $id   = $event->getParam('id', null);
+                $data = $event->getParam('data', array());
+                return $this->update($id, $data);
+            default:
+                throw new Exception\RuntimeException(sprintf(
+                    '%s has not been setup to handle the event "%s"',
+                    __METHOD__,
+                    $event->getName()
+                ));
+        }
+    }
+
+    /**
+     * Create a resource
+     * 
+     * @param  mixed $data 
+     * @return ApiProblem|mixed
+     */
+    public function create($data)
+    {
+        return new ApiProblem(405, 'The POST method has not been defined');
+    }
+
+    /**
+     * Delete a resource
+     * 
+     * @param  mixed $id 
+     * @return ApiProblem|mixed
+     */
+    public function delete($id)
+    {
+        return new ApiProblem(405, 'The DELETE method has not been defined for individual resources');
+    }
+
+    /**
+     * Delete a collection, or members of a collection
+     * 
+     * @param  mixed $data 
+     * @return ApiProblem|mixed
+     */
+    public function deleteList($data)
+    {
+        return new ApiProblem(405, 'The DELETE method has not been defined for collections');
+    }
+
+    /**
+     * Fetch a resource
+     * 
+     * @param  mixed $id 
+     * @return ApiProblem|mixed
+     */
+    public function fetch($id)
+    {
+        return new ApiProblem(405, 'The GET method has not been defined for individual resources');
+    }
+
+    /**
+     * Fetch all or a subset of resources
+     * 
+     * @param  array $params 
+     * @return ApiProblem|mixed
+     */
+    public function fetchAll($params = array())
+    {
+        return new ApiProblem(405, 'The GET method has not been defined for collections');
+    }
+
+    /**
+     * Patch (partial in-place update) a resource
+     * 
+     * @param  mixed $id 
+     * @param  mixed $data 
+     * @return ApiProblem|mixed
+     */
+    public function patch($id, $data)
+    {
+        return new ApiProblem(405, 'The PATCH method has not been defined for individual resources');
+    }
+
+    /**
+     * Replace a collection or members of a collection
+     * 
+     * @param  mixed $data 
+     * @return ApiProblem|mixed
+     */
+    public function replaceList($data)
+    {
+        return new ApiProblem(405, 'The PUT method has not been defined for collections');
+    }
+
+    /**
+     * Update a resource 
+     * 
+     * @param  mixed $id 
+     * @param  mixed $data 
+     * @return ApiProblem|mixed
+     */
+    public function update($id, $data)
+    {
+        return new ApiProblem(405, 'The PUT method has not been defined for individual resources');
+    }
+}
