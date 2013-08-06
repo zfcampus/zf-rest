2589a1cbeb49cc7ed51248f1f7c3c5108b31c762
Matthew Weier O'Phinney <matthew@zend.com>
2013-08-06 12:34:16 -0500

    Attach the ApiProblem RenderError listener
    - Attach the RenderError listener to the application event manager if a
      RestController is detected.

--- src/ZF/Rest/Module.php
+++ src/ZF/Rest/Module.php
@@ -31,4 +31,35 @@ class Module
     {
         return include __DIR__ . '/../../../config/module.config.php';
     }
+
+    /**
+     * Bootstrap listener
+     *
+     * Attaches a listener to the RestController dispatch event.
+     * 
+     * @param  \Zend\Mvc\MvcEvent $e 
+     */
+    public function onBootstrap($e)
+    {
+        $app          = $e->getTarget();
+        $events       = $app->getEventManager();
+        $sharedEvents = $events->getSharedManager();
+        $sharedEvents->attach('ZF\Rest\RestController', $e::EVENT_DISPATCH, array($this, 'onDispatch'), 100);
+    }
+
+    /**
+     * RestController dispatch listener
+     *
+     * Attach the ApiProblem RenderErrorListener when a restful controller is detected.
+     * 
+     * @param  \Zend\Mvc\MvcEvent $e 
+     */
+    public function onDispatch($e)
+    {
+        $app      = $e->getApplication();
+        $events   = $app->getEventManager();
+        $services = $app->getServiceManager();
+        $listener = $services->get('ZF\ApiProblem\RenderErrorListener');
+        $events->attach($listener);
+    }
 }
