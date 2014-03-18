0ced0d3ff5efcd59e6c750214b05197bb20380a8
Matthew Weier O'Phinney <matthew@zend.com>
2014-03-18 18:00:27 -0500

    Create OptionsListener
    - Takes configuration, and runs at "route" event at -100 priority
    - Tests to see if a route identifier was provided; if so tests against
      entity_http_methods to see if the incoming request is valid. If none
      is provided, tests against collection_http_methods.
    - Will replace functionality in the RestController.

---
+++ src/Listener/OptionsListener.php
@@ -0,0 +1,187 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ZF\Rest\Listener;
+
+use Zend\EventManager\AbstractListenerAggregate;
+use Zend\EventManager\EventManagerInterface;
+use Zend\Http\Request;
+use Zend\Http\Response;
+use Zend\Mvc\MvcEvent;
+
+class OptionsListener extends AbstractListenerAggregate
+{
+    /**
+     * @var array
+     */
+    protected $config;
+
+    /**
+     * @param  array $config
+     */
+    public function __construct(array $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * @param  EventManagerInterface $events
+     */
+    public function attach(EventManagerInterface $events)
+    {
+        $this->listeners[] = $events->attach(MvcEvent::EVENT_ROUTE, array($this, 'onRoute'), -100);
+    }
+
+    /**
+     * @param  MvcEvent $event
+     * @return void|\Zend\Http\Response
+     */
+    public function onRoute(MvcEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request instanceof Request) {
+            // Not an HTTP request? nothing to do
+            return;
+        }
+
+        $matches = $event->getRouteMatch();
+        if (!$matches) {
+            // No matches, nothing to do
+            return;
+        }
+
+        $controller = $matches->getParam('controller', false);
+        if (!$controller) {
+            // No controller in the matches, nothing to do
+            return;
+        }
+
+        if (!array_key_exists($controller, $this->config)) {
+            // No matching controller in our configuration, nothing to do
+            return;
+        }
+
+        $config  = $this->getConfigForControllerAndMatches($this->config[$controller], $matches);
+        $methods = $this->normalizeMethods($config);
+
+        $method = $request->getMethod();
+        if ($method === Request::METHOD_OPTIONS) {
+            // OPTIONS request? return response with Allow header
+            return $this->getOptionsResponse($event, $methods);
+        }
+
+        if (in_array($method, $methods)) {
+            // Valid HTTP method; nothing to do
+            return;
+        }
+
+        // Invalid method; return 405 response
+        return $this->get405Response($event, $methods);
+    }
+
+    /**
+     * Normalize an array of HTTP methods
+     *
+     * If a string is provided, create an array with that string.
+     *
+     * Ensure all options in the array are UPPERCASE.
+     *
+     * @param  string|array $methods
+     * @return array
+     */
+    protected function normalizeMethods($methods)
+    {
+        if (is_string($methods)) {
+            $methods = (array) $methods;
+        }
+
+        array_walk($methods, function (&$value) {
+            return strtoupper($value);
+        });
+        return $methods;
+    }
+
+    /**
+     * Create the Allow header
+     *
+     * @param  array $options
+     * @param  Response $response
+     */
+    protected function createAllowHeader(array $options, Response $response)
+    {
+        $headers = $response->getHeaders();
+        $headers->addHeaderLine('Allow', implode(',', $options));
+    }
+
+    /**
+     * Prepare and return an OPTIONS response
+     *
+     * Creates an empty response with an Allow header.
+     *
+     * @param  MvcEvent $event
+     * @param  array $options
+     * @return Response
+     */
+    protected function getOptionsResponse(MvcEvent $event, array $options)
+    {
+        $response = $event->getResponse();
+        $this->createAllowHeader($options, $response);
+        return $response;
+    }
+
+    /**
+     * Prepare a 405 response
+     *
+     * @param  MvcEvent $event
+     * @param  array $options
+     * @return Response
+     */
+    protected function get405Response(MvcEvent $event, array $options)
+    {
+        $response = $this->getOptionsResponse($event, $options);
+        $response->setStatusCode(405, 'Method Not Allowed');
+        return $response;
+    }
+
+    /**
+     * Retrieve the HTTP method configuration for the selected controller and request
+     *
+     * Determines if this was a request to a collection or an entity, and returns the
+     * appropriate HTTP method configuration.
+     *
+     * If an entity request was detected, but no entity configuration exists, returns
+     * 
+     * @param mixed $config 
+     * @param mixed $matches 
+     * @return void
+     */
+    protected function getConfigForControllerAndMatches($config, $matches)
+    {
+        $collectionConfig = array();
+        if (array_key_exists('collection_http_methods', $config)
+            && is_array($config['collection_http_methods'])
+        ) {
+            $collectionConfig = $config['collection_http_methods'];
+        }
+
+        $identifier = false;
+        if (array_key_exists('route_identifier_name', $config)) {
+            $identifier = $config['route_identifier_name'];
+        }
+
+        if (! $identifier || ! $matches->getParam($identifier, false)) {
+            return $collectionConfig;
+        }
+
+        if (array_key_exists('entity_http_methods', $config)
+            && is_array($config['entity_http_methods'])
+        ) {
+            return $config['entity_http_methods'];
+        }
+
+        return array();
+    }
+}
---
+++ test/Listener/OptionsListenerTest.php
@@ -0,0 +1,279 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ZFTest\Rest\Listener;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\EventManager\EventManager;
+use Zend\Http\Request;
+use Zend\Http\Response;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\RouteMatch;
+use Zend\Stdlib\Request as StdlibRequest;
+use ZF\Rest\Listener\OptionsListener;
+
+class OptionsListenerTest extends TestCase
+{
+    public function testListenerRegistersAtExpectedPriority()
+    {
+        $listener = new OptionsListener(array());
+        $events   = new EventManager();
+        $listener->attach($events);
+        $listeners = $events->getListeners('route');
+        $this->assertEquals(1, count($listeners));
+        foreach ($listeners as $test) {
+            break;
+        }
+        $this->assertInstanceOf('Zend\Stdlib\CallbackHandler', $test);
+        $this->assertEquals(array($listener, 'onRoute'), $test->getCallback());
+        $this->assertEquals(-100, $test->getMetadatum('priority'));
+    }
+
+    public function seedListenerConfig()
+    {
+        return array(
+            'controller-without-config' => array(),
+            'controller-with-entity-config' => array(
+                'route_identifier_name' => 'entity_id',
+                'entity_http_methods' => array(
+                    'GET',
+                    'PATCH',
+                    'DELETE',
+                ),
+            ),
+            'controller-with-collection-config' => array(
+                'collection_http_methods' => array(
+                    'GET',
+                    'POST',
+                ),
+            ),
+            'controller-with-all-config' => array(
+                'route_identifier_name' => 'entity_id',
+                'entity_http_methods' => array(
+                    'GET',
+                    'PATCH',
+                    'DELETE',
+                ),
+                'collection_http_methods' => array(
+                    'GET',
+                    'POST',
+                ),
+            ),
+            'controller-with-all-config-except-entity-id' => array(
+                'entity_http_methods' => array(
+                    'GET',
+                    'PATCH',
+                    'DELETE',
+                ),
+                'collection_http_methods' => array(
+                    'GET',
+                    'POST',
+                ),
+            ),
+        );
+    }
+
+    public function validMethodsProvider()
+    {
+        return array(
+            'collection-get' => array('GET', array(
+                'controller' => 'controller-with-collection-config',
+            )),
+            'collection-post' => array('POST', array(
+                'controller' => 'controller-with-collection-config',
+            )),
+            'entity-get' => array('GET', array(
+                'controller' => 'controller-with-entity-config',
+                'entity_id'  => 'foo',
+            )),
+            'entity-patch' => array('PATCH', array(
+                'controller' => 'controller-with-entity-config',
+                'entity_id'  => 'foo',
+            )),
+            'entity-delete' => array('DELETE', array(
+                'controller' => 'controller-with-entity-config',
+                'entity_id'  => 'foo',
+            )),
+            'all-collection-get' => array('GET', array(
+                'controller' => 'controller-with-all-config',
+            )),
+            'all-collection-post' => array('POST', array(
+                'controller' => 'controller-with-all-config',
+            )),
+            'all-entity-get' => array('GET', array(
+                'controller' => 'controller-with-all-config',
+                'entity_id'  => 'foo',
+            )),
+            'all-entity-patch' => array('PATCH', array(
+                'controller' => 'controller-with-all-config',
+                'entity_id'  => 'foo',
+            )),
+            'all-entity-delete' => array('DELETE', array(
+                'controller' => 'controller-with-all-config',
+                'entity_id'  => 'foo',
+            )),
+            'all-except-collection-get' => array('GET', array(
+                'controller' => 'controller-with-all-config-except-entity-id',
+            )),
+            'all-except-collection-post' => array('POST', array(
+                'controller' => 'controller-with-all-config-except-entity-id',
+            )),
+        );
+    }
+
+    public function invalidMethodsProvider()
+    {
+        return array(
+            'collection-patch' => array('PATCH', array(
+                'controller' => 'controller-with-collection-config',
+            ), array('GET', 'POST')),
+            'collection-put' => array('PUT', array(
+                'controller' => 'controller-with-collection-config',
+            ), array('GET', 'POST')),
+            'collection-delete' => array('DELETE', array(
+                'controller' => 'controller-with-collection-config',
+            ), array('GET', 'POST')),
+            'entity-post' => array('POST', array(
+                'controller' => 'controller-with-entity-config',
+                'entity_id'  => 'foo',
+            ), array('GET', 'PATCH', 'DELETE')),
+            'entity-put' => array('PUT', array(
+                'controller' => 'controller-with-entity-config',
+                'entity_id'  => 'foo',
+            ), array('GET', 'PATCH', 'DELETE')),
+            'all-collection-patch' => array('PATCH', array(
+                'controller' => 'controller-with-all-config',
+            ), array('GET', 'POST')),
+            'all-collection-put' => array('PUT', array(
+                'controller' => 'controller-with-all-config',
+            ), array('GET', 'POST')),
+            'all-collection-delete' => array('DELETE', array(
+                'controller' => 'controller-with-all-config',
+            ), array('GET', 'POST')),
+            'all-entity-post' => array('POST', array(
+                'controller' => 'controller-with-all-config',
+                'entity_id'  => 'foo',
+            ), array('GET', 'PATCH', 'DELETE')),
+            'all-entity-put' => array('PUT', array(
+                'controller' => 'controller-with-all-config',
+                'entity_id'  => 'foo',
+            ), array('GET', 'PATCH', 'DELETE')),
+            'except-collection-patch' => array('PATCH', array(
+                'controller' => 'controller-with-all-config-except-entity-id',
+            ), array('GET', 'POST')),
+            'except-collection-put' => array('PUT', array(
+                'controller' => 'controller-with-all-config-except-entity-id',
+            ), array('GET', 'POST')),
+            'except-collection-delete' => array('DELETE', array(
+                'controller' => 'controller-with-all-config-except-entity-id',
+            ), array('GET', 'POST')),
+        );
+    }
+
+    /**
+     * @dataProvider validMethodsProvider
+     */
+    public function testListenerReturnsNullWhenMethodIsAllowedForCurrentRequest($method, $matchParams)
+    {
+        $listener = new OptionsListener($this->seedListenerConfig());
+        $request  = new Request();
+        $request->setMethod($method);
+        $matches  = new RouteMatch($matchParams);
+        $mvcEvent = new MvcEvent();
+        $mvcEvent->setRequest($request);
+        $mvcEvent->setRouteMatch($matches);
+        $mvcEvent->setResponse(new Response());
+
+        $this->assertNull($listener->onRoute($mvcEvent));
+    }
+
+    /**
+     * @dataProvider invalidMethodsProvider
+     */
+    public function testListenerReturnsNullIfNotAnHttpRequest($method, $matchParams)
+    {
+        $listener = new OptionsListener($this->seedListenerConfig());
+        $request  = new StdlibRequest();
+        $matches  = new RouteMatch($matchParams);
+        $mvcEvent = new MvcEvent();
+        $mvcEvent->setRequest($request);
+        $mvcEvent->setRouteMatch($matches);
+        $mvcEvent->setResponse(new Response());
+
+        $this->assertNull($listener->onRoute($mvcEvent));
+    }
+
+    /**
+     * @dataProvider invalidMethodsProvider
+     */
+    public function testListenerReturnsNullIfNoRouteMatches($method, $matchParams, $expectedAllow)
+    {
+        $listener = new OptionsListener($this->seedListenerConfig());
+        $request  = new Request();
+        $request->setMethod($method);
+        $mvcEvent = new MvcEvent();
+        $mvcEvent->setRequest($request);
+        $mvcEvent->setResponse(new Response());
+
+        $this->assertNull($listener->onRoute($mvcEvent));
+    }
+
+    public function testListenerReturnsNullIfNoMatchingControllerInRouteMatches()
+    {
+        $listener = new OptionsListener($this->seedListenerConfig());
+        $request  = new Request();
+        $request->setMethod('GET');
+        $matches  = new RouteMatch(array());
+        $mvcEvent = new MvcEvent();
+        $mvcEvent->setRequest($request);
+        $mvcEvent->setResponse(new Response());
+
+        $this->assertNull($listener->onRoute($mvcEvent));
+    }
+
+    public function testListenerReturnsNullIfMatchingControllerInRouteMatchesButNoConfigForController()
+    {
+        $listener = new OptionsListener($this->seedListenerConfig());
+        $request  = new Request('GET');
+        $matches  = new RouteMatch(array(
+            'controller' => 'controller-without-config',
+        ));
+        $mvcEvent = new MvcEvent();
+        $mvcEvent->setRequest($request);
+
+        $this->assertNull($listener->onRoute($mvcEvent));
+    }
+
+    /**
+     * @dataProvider invalidMethodsProvider
+     */
+    public function testListenerReturns405ResponseWithAllowHeaderForInvalidRequestMethod($method, $matchParams, $expectedAllow)
+    {
+        $listener = new OptionsListener($this->seedListenerConfig());
+        $request  = new Request();
+        $request->setMethod($method);
+        $matches  = new RouteMatch($matchParams);
+        $mvcEvent = new MvcEvent();
+        $mvcEvent->setRequest($request);
+        $mvcEvent->setRouteMatch($matches);
+        $mvcEvent->setResponse(new Response());
+
+        $result = $listener->onRoute($mvcEvent);
+        $this->assertInstanceOf('Zend\Http\Response', $result);
+        $this->assertEquals(405, $result->getStatusCode());
+        $headers = $result->getHeaders();
+        $this->assertTrue($headers->has('Allow'));
+        $allow = $headers->get('Allow');
+        $allow = $allow->getFieldValue();
+        $allow = explode(',', $allow);
+        array_walk($allow, function (&$value) {
+            $value = trim($value);
+        });
+        sort($allow);
+        sort($expectedAllow);
+        $this->assertEquals($expectedAllow, $allow);
+    }
+}
