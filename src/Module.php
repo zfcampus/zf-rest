960e36fc8c859a25d406fac3bcdbdf17f81a38ad
Matthew Weier O'Phinney <matthew@zend.com>
2016-07-11 16:08:34 -0500

    Updated composer requirements
    - Removed any that were not actually used in source or tests.
    - Updated all others to most recent stable versions, using `||` notation for
      those with both v2 and v3 releases.
    - Updated PHP requirement to use 5.6 as minimum supported version.
    - Moved `Module` class into `src/` tree, where it belongs, and removed
      `getAutoloaderConfig()` implementation (allowing removal of zend-loader
      dependency).

--- src/Module.php
+++ src/Module.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
- * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @copyright Copyright (c) 2014-2016 Zend Technologies USA Inc. (http://www.zend.com)
  */
 
 namespace ZF\Rest;
@@ -12,25 +12,13 @@ namespace ZF\Rest;
 class Module
 {
     /**
-     * Retrieve autoloader configuration
-     *
-     * @return array
-     */
-    public function getAutoloaderConfig()
-    {
-        return array('Zend\Loader\StandardAutoloader' => array('namespaces' => array(
-            __NAMESPACE__ => __DIR__ . '/src/',
-        )));
-    }
-
-    /**
      * Retrieve module configuration
      *
      * @return array
      */
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 
     /**
